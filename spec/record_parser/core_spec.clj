4f01820b83d8a4df28fbec31df0452cdc13636c0
bwoodfork <bwoodfork@8thlight.com>
2017-08-25 19:13:09 -0500

    initial commit

---
+++ spec/record_parser/core_spec.clj
@@ -0,0 +1,7 @@
+(ns record-parser.core-spec
+  (:require [speclj.core :refer :all]
+            [record-parser.core :refer :all]))
+
+(describe "a test"
+  (it "FIXME, I fail."
+    (should= 0 1)))
---
+++ src/record_parser/core.clj
@@ -0,0 +1,5 @@
+(ns record-parser.core)
+
+(defn -main
+  [& args]
+  (println "Hello World"))
